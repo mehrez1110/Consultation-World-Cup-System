c1afb6f5f08e8631b78d68a66abaeabbf9e5aea5
Mustafa Usama <mustafausamaa@gmail.com>
2023-01-01 23:06:13 +0200

    Update match-controller.dart

--- world_cup_system_frontend_web/lib/controllers/match-controller.dart
+++ world_cup_system_frontend_web/lib/controllers/match-controller.dart
@@ -57,4 +57,62 @@ class MatchController extends MomentumController<MatchModel> {
       print(e);
     }
   }
+
+  Future<void> getTeams(context) async {
+    try {
+      var url = Uri.http(STAGING_URL, "/api/team");
+      http.Response response = await http.get(url, headers: {
+        'Authorization':
+            'Bearer ${Momentum.controller<AuthController>(context).model.tempToken}',
+        "Access-Control-Allow-Origin": "*",
+        // 'Content-Type': 'application/json',
+        'Accept': '*/*'
+      });
+      if (response.statusCode == 200) {
+        var jsonResponse = convert.jsonDecode(response.body) as List<dynamic>;
+        List teams = [];
+        List<String> names = [];
+        for (var responseItem in jsonResponse) {
+          final match = Team(
+            id: responseItem["id"],
+            name: responseItem["name"],
+            winsCount: responseItem["winsCount"],
+            drawsCount: responseItem["drawsCount"],
+            lossCount: responseItem["lossCount"],
+            rating: responseItem["rating"],
+          );
+          teams.add(match);
+          names.add(match.name);
+        }
+        print(names);
+        model.update(teamsList: teams, teamNames: names);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> updateMatch(context, int id) async {
+    try {
+      var url = Uri.http(STAGING_URL, "/api/matches/", {"id": id.toString()});
+      http.Response response = await http.post(url, headers: {
+        'Authorization':
+            'Bearer ${Momentum.controller<AuthController>(context).model.tempToken}',
+        "Access-Control-Allow-Origin": "*",
+        // 'Content-Type': 'application/json',
+        'Accept': '*/*'
+      }, body: {
+        "homeTeam": model.firstTeam,
+        "awayTeam": model.secondTeam,
+        "matchStadium": model.stadium,
+        "matchDate": model.dateTime,
+        "seatsCount": model.seatsLeft,
+        "lineManA": model.firstLinesman,
+        "lineManB": model.secondLinesman,
+        "referee": model.mainReferee,
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
 }
